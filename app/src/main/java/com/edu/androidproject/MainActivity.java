c8c4117d231eea432c62388219acef19e3d4df85
JudexMars <danielpro2013rus@gmail.com>
2023-02-17 13:36:14 +0300

    Added some logging for the whole activity life cycle // Добавил логгирование для всех этапов жизненного цикла активности

--- app/src/main/java/com/edu/androidproject/MainActivity.java
+++ app/src/main/java/com/edu/androidproject/MainActivity.java
@@ -3,9 +3,12 @@ package com.edu.androidproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,41 +16,48 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Toast.makeText(this.getApplicationContext(), "onCreate()", Toast.LENGTH_SHORT).show();
+        Log.i(TAG, "Активность создана");
     }
 
     @Override
     protected void onStart() {
         super.onStart();
         Toast.makeText(this.getApplicationContext(), "onStart()", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "Активность стартовала");
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
         Toast.makeText(this.getApplicationContext(), "onRestart()", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "Активность перезапущена");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
         Toast.makeText(this.getApplicationContext(), "onPause()", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "Активность приостановлена");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         Toast.makeText(this.getApplicationContext(), "onResume()", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "Активность возобновлена");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
         Toast.makeText(this.getApplicationContext(), "onStop()", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "Активность остановлена");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         Toast.makeText(this.getApplicationContext(), "onDestroy()", Toast.LENGTH_SHORT).show();
+        Log.w(TAG, "Активность уничтожена");
     }
 }
