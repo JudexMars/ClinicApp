1ef0f3d7a3d1d4f8b02abe71cef22b9aae8db464
JudexMars <danielpro2013rus@gmail.com>
2023-02-24 23:29:19 +0300

    More closer connection between login and signup activities // Реализовал передачу данных между окном входа и окном регистрации, а также добавил обработчики событий для кнопок

--- app/src/main/java/com/edu/androidproject/MainActivity.java
+++ app/src/main/java/com/edu/androidproject/MainActivity.java
@@ -1,14 +1,40 @@
 package com.edu.androidproject;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
+
+    ActivityResultLauncher<Intent> mStartForRegData = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Intent intent = result.getData();
+                        Bundle res = intent.getExtras();
+                        ((EditText)findViewById(R.id.usernameText)).setText(res.getString("email"));
+                        ((EditText)findViewById(R.id.passwordText)).setText(res.getString("password"));
+                    }
+                }
+            }
+    );
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +43,26 @@ public class MainActivity extends AppCompatActivity {
 
         Toast.makeText(this.getApplicationContext(), "onCreate()", Toast.LENGTH_SHORT).show();
         Log.i(TAG, "Активность создана");
+
+        Button signupButton = (Button)findViewById(R.id.signUpButton);
+        signupButton.setText(R.string.signup_button_text);
+
+        ImageView caduceus = (ImageView)findViewById(R.id.caduceusImage);
+        caduceus.setImageResource(R.drawable.caduceus);
+
+        Button loginButton = (Button) findViewById(R.id.loginButton);
+        loginButton.setOnClickListener((View v) -> {
+            Log.i(TAG, "Кнопка входа была нажата");
+            startActivity(new Intent(this, Home.class));
+        });
+    }
+
+    public void signUp_onClick(View v ) {
+        Log.i(TAG, "Кнопка регистрации была нажата");
+
+        Intent intent = new Intent(this, Registration.class);
+        intent.putExtra("email", ((EditText)findViewById(R.id.usernameText)).getText().toString());
+        mStartForRegData.launch(intent);
     }
 
     @Override
--- app/src/main/java/com/edu/androidproject/Registration.java
+++ app/src/main/java/com/edu/androidproject/Registration.java
@@ -2,7 +2,13 @@ package com.edu.androidproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.SpannableString;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Registration extends AppCompatActivity {
 
@@ -10,5 +16,23 @@ public class Registration extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        ((EditText)findViewById(R.id.emailTextField)).setText(getIntent().getExtras().getString("email"));
+
+        Button signup_button = (Button) findViewById(R.id.registerBtn);
+        signup_button.setOnClickListener((View v) -> {
+            Intent input = getIntent();
+            Intent res = new Intent();
+            String email = input.getExtras().getString("email");
+
+            Bundle resBundle = new Bundle();
+            resBundle.putString("email", ((EditText)(findViewById(R.id.emailTextField))).getText().toString());
+            resBundle.putString("password", ((EditText)(findViewById(R.id.passwordTextField))).getText().toString());
+
+            res.putExtras(resBundle);
+
+            setResult(RESULT_OK, res);
+            finish();
+        });
     }
 }
