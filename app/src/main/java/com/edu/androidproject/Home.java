692b0286f4af71dab273d1dc6191d8b697a78d9d
JudexMars <danielpro2013rus@gmail.com>
2023-02-25 19:26:15 +0300

    Added additional logging for home activity // Добавил дополнительное логгирование и метод onClick для домашней Activity

--- app/src/main/java/com/edu/androidproject/Home.java
+++ app/src/main/java/com/edu/androidproject/Home.java
@@ -3,6 +3,9 @@ package com.edu.androidproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class Home extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Button loginButton = (Button) findViewById(R.id.newAppointmentBtn);
+        loginButton.setOnClickListener((View v) -> {
+            Log.i("Home Activity", "Была нажата кнопка создания записи");
+        });
     }
 }
