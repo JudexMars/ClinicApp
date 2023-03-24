2d0f310f49e1aa4e27e93e12bbe97289595e8354
JudexMars <danielpro2013rus@gmail.com>
2023-03-24 23:33:42 +0300

    Added one notification // Добавил уведомление

--- app/src/main/java/com/edu/androidproject/HomeFragment.java
+++ app/src/main/java/com/edu/androidproject/HomeFragment.java
@@ -1,9 +1,17 @@
 package com.edu.androidproject;
 
 import android.annotation.SuppressLint;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.util.Log;
@@ -11,6 +19,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.Manifest;
 
 import com.edu.androidproject.databinding.FragmentHomeBinding;
 
@@ -38,10 +47,58 @@ public class HomeFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstance) {
         super.onViewCreated(view, savedInstance);
 
-        Button loginButton = binding.newAppointmentBtn;
-        loginButton.setOnClickListener((View v) -> Log.i(TAG, "Была нажата кнопка создания записи"));
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel("My notification",
+                    "My notification", NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager manager = getActivity().getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
+
+        Button apptButton = binding.newAppointmentBtn;
+        apptButton.setOnClickListener((View v) ->
+        {
+            showNotification();
+            Log.i(TAG, "Была нажата кнопка создания записи"); });
 
         String userName = this.getArguments().getString("name");
         binding.welcomeTextView.setText("Добро пожаловать, " + userName);
     }
+
+    private final String CHANNEL_ID = "channel_id";
+    private final int PERMISSION_REQUEST_CODE = 0;
+
+    private void showNotification() {
+        if (ContextCompat.checkSelfPermission(getContext(),
+                Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED) {
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(getContext(),
+                    "My notification")
+                    .setSmallIcon(R.drawable.caduceus)
+                    .setContentTitle(getString(
+                            R.string.notification_title))
+                    .setContentText("Sample text")
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+            NotificationManagerCompat notificationManager =
+                    NotificationManagerCompat.from(getContext());
+            notificationManager.notify(0, builder.build());
+        }
+        else requestPermissions();
+    }
+
+    private void requestPermissions() {
+        ActivityCompat.requestPermissions(getActivity(),
+                new String[] {
+                        Manifest.permission.POST_NOTIFICATIONS
+                },
+            PERMISSION_REQUEST_CODE);
+    }
+
+//    @Override
+//    public void onRequestPermissionResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+//        if (requestCode == PERMISSION_REQUEST_CODE && grantResults.length == 1) {
+//            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//
+//            }
+//        }
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+//    }
 }
